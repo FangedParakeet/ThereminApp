9d326ba672c35983594d1f4c1d22520cc8bcfb6b
Samir Merchant <fanged.parakeet@gmail.com>
2014-04-14 12:15:40 +0530

    debut commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThereminApp::Application.config.secret_token = 'f35e92b111fe7718da16fe456956e341d333be56418d433603f221f3652c1db4455dc098e967300712f0e75706beafe44178803095bbd9a6ff72b922360ac754'
